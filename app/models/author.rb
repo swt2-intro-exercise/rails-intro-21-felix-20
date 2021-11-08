3f246be35c6dbbb39ea68cc2a9d38e978b4e2b7c
felix-20 <felix-20@franken20.de>
2021-11-08 00:25:48 -0800

    added author class

---
+++ app/models/author.rb
@@ -0,0 +1,24 @@
+class Author #< ApplicationRecord
+
+    def initialize(first, last, page)
+        @first_name = first
+        @last_name = last
+        @homepage = page
+    end
+
+    def first_name
+        @first_name
+    end
+
+    def last_name
+        @last_name
+    end
+
+    def homepage
+        @homepage
+    end
+
+    def name()
+        first_name + " " + last_name
+    end
+end
--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -4,6 +4,6 @@ describe "author", type: :model do
     expect(author.first_name).to eq("Alan")
     expect(author.last_name).to eq("Turing")
     expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
-    expect(author.name()).to.eq("Alan Turing")
+    expect(author.name()).to eq("Alan Turing")
   end
 end
