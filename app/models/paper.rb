ac47c79a528bf6d292f4cb7f697507701e16ff3f
felix-20 <felix-20@franken20.de>
2021-11-18 06:25:12 -0800

    added author selection for paper #16

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,11 +3,4 @@ class Paper < ApplicationRecord
     validates :venue, presence: true
     validates :year, presence: true, numericality: true, :inclusion => 0..2100
     has_and_belongs_to_many :authors
-
-    after_initialize :init
-
-    def init
-        self.authors ||= []
-    end
-
 end
