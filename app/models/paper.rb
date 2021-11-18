6a445b7c0019e14e3e63af44516cb06a372f704f
felix-20 <felix-20@franken20.de>
2021-11-18 04:29:09 -0800

    added year input field validation

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -1,5 +1,5 @@
 class Paper < ApplicationRecord
     validates :title, presence: true
     validates :venue, presence: true
-    validates :year, presence: true
+    validates :year, presence: true, numericality: true, :inclusion => 0..2100
 end
