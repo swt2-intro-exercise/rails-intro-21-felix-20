7cea04cb305d14ad4256ec3d3fbd81285187e387
felix-20 <felix-20@franken20.de>
2021-11-18 05:41:55 -0800

    one more try for #14

--- db/migrate/20211112134632_create_papers.rb
+++ db/migrate/20211112134632_create_papers.rb
@@ -1,7 +1,7 @@
 class CreatePapers < ActiveRecord::Migration[6.0]
   def change
     create_table :papers do |t|
-      t.has_many :authors
+      # t.has_many :authors
       t.string :title
       t.string :venue
       t.integer :year
