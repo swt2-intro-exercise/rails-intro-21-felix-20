978264259af26fdef6326a0437232951c8487b62
felix-20 <felix-20@franken20.de>
2021-11-18 05:23:24 -0800

    try for #14

--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -2,6 +2,6 @@ describe "author", type: :model do
   
   it "should have an empty author list" do
     paper = Paper.new(title: "COMPUTING MACHINERY AND INTELLIGENCE", venue: "Mind 49: 433-460", year: 1950)
-    expect(paper[:authors]).to be_empty
+    expect(paper[:authors]).to be_nil
   end
 end
