c4ba3782de9c87fe1cd45341c12ebeff8ea057c5
felix-20 <felix-20@franken20.de>
2021-11-18 06:43:47 -0800

    added year parameter

--- app/controllers/papers_controller.rb
+++ app/controllers/papers_controller.rb
@@ -3,7 +3,11 @@ class PapersController < ApplicationController
 
   # GET /papers
   def index
-    @papers = Paper.all
+    if params[:year]
+      @papers = Paper.published_in(params[:year])
+    else
+      @papers = Paper.all
+    end
   end
 
   # GET /papers/1
--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,6 @@ class Paper < ApplicationRecord
     validates :venue, presence: true
     validates :year, presence: true, numericality: true, :inclusion => 0..2100
     has_and_belongs_to_many :authors
+
+    scope :published_in, ->(year) { where("year == ?", year) if year.present? }
 end
